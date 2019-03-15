a5ccb96344e12bcc2a40a3a64fa994870e27f5ad
forcheng <2871979271@qq.com>
2019-03-15 11:13:58 +0800

    [forcheng] add a demo for `AutoCompleteSuffixSelect` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import InputNumber from "./component/input/inputNumber";
+import SelectEmail from "./component/select/autoCompleteSuffixSelect";
+import 'antd/dist/antd.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         只能输入数字的输入框：<InputNumber />
+        邮箱后缀自动补齐：<SelectEmail style={{ width: 200 }} suffixes={['@qq.com', '@baidu.com', '@tw.com', '@me.com']}/>
       </div>
     );
   }
